4018285e0dcfd20cd8ba1d084c004ea086aaf7a6
Luke Wyman <c-luke.wyman@charter.com>
2023-02-16 21:50:57 -0700

    completed fastapi backend service

---
+++ fastapi-backend-service/app/thumbnail_maker.py
@@ -0,0 +1,22 @@
+from PIL import Image
+from io import BytesIO
+import requests
+import os 
+
+from fastapi import HTTPException
+from requests.models import MissingSchema
+from PIL import UnidentifiedImageError
+
+STATIC_DIR = os.environ.get("STATIC_DIR", "/tmp/static")
+SIZE = 128, 128
+
+def create(url, filename):
+    try:
+        content = requests.get(url).content 
+        with Image.open(BytesIO(content)) as img:
+            img.thumbnail(SIZE)
+            img.save(f'{STATIC_DIR}/{filename}', 'JPEG')
+    except UnidentifiedImageError as uie:
+        raise HTTPException(status_code=400, detail="Invalid input")
+    except MissingSchema as ms:
+        raise HTTPException(status_code=400, detail="Invalid input")
